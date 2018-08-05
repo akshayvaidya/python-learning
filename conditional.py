bb4a23f9582b0232a3d90d7ca4dbfaa64216f0f3
Akshay Vaidya <akshayvaidya23@gmail.com>
2018-08-05 21:54:15 +0530

    second

---
+++ conditional.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  5 21:52:40 2018
+
+@author: akshay
+"""
+
+x = 5
+if x >= 5:
+    print("not here")
+elif x == 5 :
+    print("here")
+else:
+    print("strange")
