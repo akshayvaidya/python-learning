3a5ef9a48e50c5ecc348b3ba8ce9dc6feba5e93f
Akshay Vaidya <akshayvaidya23@gmail.com>
2018-08-05 21:50:17 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 07:39:37 2018
+
+@author: akshay
+"""
+
+print("hello world")
+x = int(input('enter value : '))
+if (x+5) > 10 :
+    print('greater than 10')
+elif (x+5) > 5 :
+    print('greater than zero')
+else :
+    print('negative')
+print('end')    
